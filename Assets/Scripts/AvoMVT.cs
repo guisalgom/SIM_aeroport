821c4d5104ac9e4a6641d9b5a4bf3a43e59f00a1
Jordi Jounou Beraza <jordi.jounou.beraza@est.fib.upc.edu>
2017-06-11 05:48:39 +0200

    avions creats i moguts

---
+++ Assets/Scripts/AvoMVT.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoMVT : MonoBehaviour {
+
+    int porta;
+    private GameObject controlador;
+    private ControlPorta porta_id;
+    private float porta_dec_x;
+    private float porta_dec_z;
+    public float speed = 10f;
+    public int estat = 0;
+
+    // Use this for initialization
+    void Start () {
+
+        controlador = GameObject.FindGameObjectWithTag("GameController");
+        
+
+        //porta = Mathf.RoundToInt(Random.Range(1, 12));
+        
+
+        //porta_id = GameObject.Find("Porta (" + porta.ToString() + ")").GetComponent<ControlPorta>();
+        //porta_dec_x = Random.Range(-10, 10);
+        //porta_dec_z = Random.Range(-10, 10);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        print("estat " + estat);
+
+        if (estat == 0) {
+            porta = controlador.GetComponent<CreaAvions>().get_porta();
+            print("porta " + porta);
+            if (porta != -1) {
+                porta_id = GameObject.Find("Porta (" + porta.ToString() + ")").GetComponent<ControlPorta>();
+                if (porta > 6)
+                {
+                    porta_dec_x = 0;
+                    porta_dec_z = 12;
+                }
+                else {
+                    porta_dec_x = 12;
+                    porta_dec_z = 0;
+
+                }
+                    estat = 1;
+            }
+        }
+        else if (estat == 1) {
+            anar_a_la_porta();
+        }
+       
+	}
+
+    void Ocupacio(int porta_desti) {
+        porta = porta_desti;
+    }
+
+    void anar_a_la_porta()
+    {
+        Vector3 porta_pos = porta_id.transform.position;
+        Vector3 my_pos = transform.position;
+
+        if (porta_pos.x + porta_dec_x < my_pos.x) { my_pos = new Vector3(my_pos.x - speed * Time.deltaTime, my_pos.y, my_pos.z); }
+        if (porta_pos.x + porta_dec_x > my_pos.x) { my_pos = new Vector3(my_pos.x + speed * Time.deltaTime, my_pos.y, my_pos.z); }
+        if (porta_pos.z + porta_dec_z < my_pos.z) { my_pos = new Vector3(my_pos.x, my_pos.y, my_pos.z - speed * Time.deltaTime); }
+        if (porta_pos.z + porta_dec_z > my_pos.z) { my_pos = new Vector3(my_pos.x, my_pos.y, my_pos.z + speed * Time.deltaTime); }
+        transform.position = my_pos;
+    }
+}
---
+++ Assets/Scripts/CreaAvions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaAvions : MonoBehaviour {
+
+    public float frequencia = 5;
+    private float temps;
+    private Object avio;
+    private bool[] disponibilitat_portes;
+    private bool porta_lliure;
+
+    // Use this for initialization
+    void Start () {
+
+        temps = 0;
+
+        avio = Resources.Load("obj_avio");
+
+        disponibilitat_portes = new bool[12] { false, false, false, false, false, false, false, false, false, false, false, false };
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        temps += Time.deltaTime;
+        if (temps > frequencia) { make_avions(); temps = 0; }
+
+    }
+
+    void make_avions() {
+        GameObject myRoadInstance = Instantiate(avio, new Vector3(Random.Range(130f, 9f), 2.5f, 188), Quaternion.identity) as GameObject;
+    }
+
+    bool comprova_disponibilitat() {
+        for (int i = 0; i < disponibilitat_portes.Length; i++) {
+            if (!disponibilitat_portes[i]) return true;
+        }
+        return false;
+    }
+
+    public int get_porta () {
+        int porta_desti = -1;
+        print ("disponibilitat " + comprova_disponibilitat());
+        if (comprova_disponibilitat()) {
+            print("lliure");
+            bool trobat = false;
+            while (!trobat) { 
+                int porta = Mathf.RoundToInt(Random.Range(0, 12));
+                if (!disponibilitat_portes[porta])
+                {
+                    disponibilitat_portes[porta] = true;
+                    trobat = true;
+                    porta_desti = porta+1;
+                }
+            }
+        }
+        return porta_desti;
+    }
+}
